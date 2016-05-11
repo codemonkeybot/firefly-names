928a7e6a94544a81b04db818d24af9520be46c5c
codemonkeybot <rich.cunday@gmail.com>
2016-05-11 12:33:04 -0600

    Import Lib Bug Fix

--- src/index.js
+++ src/index.js
@@ -1,21 +1,9 @@
-import uniqueRandomArray from 'unique-random-array';
-import fireFlyNames from './firefly-names.json';
+var uniqueRandomArray = require('unique-random-array');
+var fireFlyNames = require('./firefly-names.json');
 
 var getRandomItem = uniqueRandomArray(fireFlyNames);
 
 module.exports = {
   all: fireFlyNames,
-  random: random
+  random: uniqueRandomArray(fireFlyNames)
 };
-
-function random(number) {
-  if (number === undefined) {
-    return getRandomItem();
-  } else {
-    var randomItems = [];
-    for (var i = 0; i < number; i++) {
-      randomItems.push(getRandomItem());
-    }
-    return randomItems;
-  }
-}
